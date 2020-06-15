fd0bf7dd494c856901149bd706a22f136f8f061e
Jordan Yee <jyee117@gmail.com>
2020-06-15 12:57:33 -0600

    Update backup script outputs

--- backup-configs.sh
+++ backup-configs.sh
@@ -2,12 +2,12 @@
 # Backup configs from the home directory to dangerscripts
 
 cp -u ~/.zshrc ~/dangerscripts/.zshrc
-echo ".zshrc has been copied to dangerscripts"
+echo ".zshrc has been copied to dangerscripts..."
 
 cp -u ~/.tmux.conf ~/dangerscripts/.tmux.conf
-echo ".tmux.conf has been copied to dangerscipts"
+echo ".tmux.conf has been copied to dangerscipts..."
 
 cp -u ~/.config/kak/kakrc ~/dangerscripts/kakrc
-echo "kakrc has been copied to dangerscipts"
+echo "kakrc has been copied to dangerscipts..."
 
-echo "Config backup complete."
+echo "\nConfig backup complete."
