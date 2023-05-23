c4e4703e8cc2f4036b141a01128cce3b722f7d9c
Jordan Yee <jordan.yee@dividendfinance.com>
2023-05-23 11:47:42 -0600

    Update output of difflocal comparison between kakoune rc directories

--- difflocal.sh
+++ difflocal.sh
@@ -1,6 +1,6 @@
 diff --brief kakoune/kakrc ~/.config/kak/kakrc
 diff --brief --recursive kakoune/custom ~/.config/kak/custom
-diff --brief --recursive kakoune/rc /usr/local/share/kak/rc
+diff --brief --recursive kakoune/rc /usr/local/share/kak/rc | grep -v 'Only in /usr/local/share/kak/rc'
 diff --brief mintty/.minttyrc ~/.minttyrc
 diff --brief sakura/sakura.conf ~/.config/sakura/sakura.conf
 diff --brief tmux/.tmux.conf ~/.tmux.conf
