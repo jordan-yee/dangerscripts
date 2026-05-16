c221ce7b771959208349a7904cc451ff94ab350a
Jordan Yee <jyee117@gmail.com>
2026-05-16 16:33:22 -0600

    Update difflocal script targets

--- difflocal.sh
+++ difflocal.sh
@@ -1,8 +1,9 @@
-diff --brief kakoune/kakrc ~/.config/kak/kakrc
-diff --brief kakoune/kakrc-*.kak ~/.config/kak/kakrc-*.kak
-diff --brief --recursive kakoune/custom ~/.config/kak/custom
-diff --brief --recursive kakoune/highlighters ~/.config/kak/highlighters
-diff --brief --recursive kakoune/rc /usr/local/share/kak/rc | grep -v 'Only in /usr/local/share/kak/rc'
+diff --brief kakoune-user/.config/kak/kakrc ~/.config/kak/kakrc
+diff --brief kakoune-user/.config/kak/kakrc-*.kak ~/.config/kak/kakrc-*.kak
+diff --brief --recursive kakoune-user/.config/kak/custom ~/.config/kak/custom
+diff --brief --recursive kakoune-user/.config/kak/highlighters ~/.config/kak/highlighters
+diff --brief --recursive kakoune-user/.config/kak/plugins ~/.config/kak/plugins
+diff --brief --recursive kakoune-local/share/kak/rc /usr/local/share/kak/rc | grep -v 'Only in /usr/local/share/kak/rc'
 diff --brief tmux/.tmux.conf ~/.tmux.conf
 diff --brief zshell/.zshrc ~/.zshrc
 diff --brief --recursive flowstorm ~/.flow-storm
