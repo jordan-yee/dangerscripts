e64c15698e6f875843554e2e201c2377157bbde8
Jordan Yee <jyee117@gmail.com>
2026-05-19 13:59:44 -0600

    Fix difflocal script: handle multiple kakrc's & remove check for plugins dir

--- difflocal.sh
+++ difflocal.sh
@@ -1,8 +1,7 @@
 diff --brief kakoune-user/.config/kak/kakrc ~/.config/kak/kakrc
-diff --brief kakoune-user/.config/kak/kakrc-*.kak ~/.config/kak/kakrc-*.kak
+for f in kakoune-user/.config/kak/kakrc-*.kak; do diff --brief "$f" ~/.config/kak/"${f##*/}"; done
 diff --brief --recursive kakoune-user/.config/kak/custom ~/.config/kak/custom
 diff --brief --recursive kakoune-user/.config/kak/highlighters ~/.config/kak/highlighters
-diff --brief --recursive kakoune-user/.config/kak/plugins ~/.config/kak/plugins
 diff --brief --recursive kakoune-local/share/kak/rc /usr/local/share/kak/rc | grep -v 'Only in /usr/local/share/kak/rc'
 diff --brief tmux/.tmux.conf ~/.tmux.conf
 diff --brief zshell/.zshrc ~/.zshrc
